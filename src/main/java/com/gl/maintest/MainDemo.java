cb0d23044beea5c13ea3868541e60b339d29116b
yiyexiaocao <geliang_gnaileg@163.com>
2016-11-12 14:09:01 +0800

    hha

---
+++ src/main/java/com/gl/maintest/MainDemo.java
@@ -0,0 +1,43 @@
+package com.gl.maintest;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * Created by gl on 2016/11/9.
+ */
+public class MainDemo {
+
+
+    public static void main(String[] args) {
+
+        File directory  ;
+        File file  ;
+        try {
+            String fileName = "C:\\Users\\gl\\Desktop";
+            directory = new File(fileName);
+            directory.mkdirs();
+            if (!directory.exists()) {
+                return;
+            }
+            file = new File(fileName + File.separator + "Date.txt");
+            file.createNewFile();
+            if (!file.isFile()) {
+                return;
+            }
+            FileOutputStream outputStream = new FileOutputStream(file);
+            FileChannel channel = outputStream.getChannel();
+            ByteBuffer byteBuffer = ByteBuffer.allocate(2014);
+            String str = "geliang geliang geliang..";
+            byteBuffer.put(str.getBytes());
+            byteBuffer.flip();
+            channel.write(byteBuffer);
+            channel.close();
+            outputStream.close();
+        } catch (Exception e) {
+
+        }
+
+    }
+}
